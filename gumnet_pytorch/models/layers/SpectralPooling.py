787085b2f56f8167a9297d5b6d954e742043570f
jddaggett <jddaggett@github.com>
2024-05-24 19:32:42 +0000

    using torch.fft again for faster pooling directly on GPU

--- gumnet_pytorch/models/layers/SpectralPooling.py
+++ gumnet_pytorch/models/layers/SpectralPooling.py
@@ -1,10 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from scipy.fftpack import dctn, idctn
+import numpy as np
 
-# @TODO check if the 3d dct and idct functions are working with the CUDA tensors
-# when the GPU is not at nearly 100% memory usage
 class SpectralPooling(nn.Module):
     """
     This class implements spectral pooling and filtering using 3D DCT, designed to work with noisy 3D image data.
@@ -16,6 +14,8 @@ class SpectralPooling(nn.Module):
         self.homomorphic = homomorphic  # If True, apply logarithmic and exponential transformations
 
     def forward(self, x):
+        x = x.to(torch.float32)
+
         # Apply logarithmic transformation if homomorphic processing is enabled.
         if self.homomorphic:
             x = torch.log(x + 1e-6)  # Adding a small constant to prevent log(0)
@@ -32,14 +32,16 @@ class SpectralPooling(nn.Module):
         return x_idct
     
     def _dct3D(self, x):
-        x_np = x.cpu().detach().numpy()
-        x_dct = dctn(x_np, norm='ortho')
-        return torch.tensor(x_dct, device=x.device)
+        # Perform DCT using FFT
+        x = torch.fft.fftn(x, dim=(2, 3, 4), norm='ortho')
+        x = torch.real(x)
+        return x
 
     def _idct3D(self, x):
-        x_np = x.cpu().detach().numpy()
-        x_idct = idctn(x_np, norm='ortho')
-        return torch.tensor(x_idct, device=x.device)
+        # Perform inverse DCT using FFT
+        x = torch.fft.ifftn(x, dim=(2, 3, 4), norm='ortho')
+        x = torch.real(x)
+        return x
 
     def _cropping3D(self, x):
         # Crop the high-frequency components based on the truncation settings
