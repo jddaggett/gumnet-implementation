d218f62909ec1762b60eafdbe14b2bf8e5ae6a4b
jddaggett <jddaggett@github.com>
2024-06-19 01:01:29 +0000

    generalized for multiple datasets

--- gumnet_pytorch/process_data.py
+++ gumnet_pytorch/process_data.py
@@ -31,23 +31,7 @@ def euler_to_rot_matrix(theta):
     R = R.transpose(1, 2)
     return R 
 
-# Generates usable data by the model from the GroEL-ES dataset
-def load_GroEL_ES():
-    # Load data
-    with open('../dataset/GroEl-ES.pickle', 'rb') as f:
-        data = pickle.load(f, encoding='latin1')
-
-    # Shuffle data
-    np.random.shuffle(data)
-
-    # Split data as defined in GumNet paper
-    num_training = 617
-    num_validation = 69
-    num_test = 100
-    train_data = data[:num_training]
-    valid_data = data[num_training:num_training + num_validation]
-    test_data = data[num_training + num_validation:num_training + num_validation + num_test]
-
+def generate_data(train_data, valid_data, test_data):
     # Convert data to torch tensors and reshape to [B, C, D, H, W]
     device = device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     train_x = torch.tensor(train_data, dtype=torch.float32).permute(0, 4, 1, 2, 3).to(device)
@@ -73,11 +57,11 @@ def load_GroEL_ES():
         return grid
 
     # Generate ground truth 6D transformation parameters for evaluating the model
-    ground_truth = torch.rand(num_test, 6)
+    ground_truth = torch.rand(test_data.shape[0], 6)
 
     # Generate and sample affine grids to get target subtomagrams
-    train_grid = gen_grid(train_x, torch.rand(num_training, 6))
-    valid_grid = gen_grid(valid_x, torch.rand(num_validation, 6))
+    train_grid = gen_grid(train_x, torch.rand(train_data.shape[0], 6))
+    valid_grid = gen_grid(valid_x, torch.rand(valid_data.shape[0], 6))
     test_grid = gen_grid(test_x, ground_truth) # transform the test by the ground truth params
     train_y = F.grid_sample(train_x, train_grid, align_corners=False, mode='bilinear', padding_mode='zeros')
     valid_y = F.grid_sample(valid_x, valid_grid, align_corners=False, mode='bilinear', padding_mode='zeros')
@@ -88,3 +72,35 @@ def load_GroEL_ES():
     missing_mask = torch.zeros(32, 32, 32).to(device).unsqueeze(0).unsqueeze(0).repeat(train_x.shape[0], 1, 1, 1, 1)
 
     return train_x, train_y, valid_x, valid_y, test_x, test_y, observed_mask, missing_mask, ground_truth
+
+# Generates usable torch data from the GroEL-ES dataset
+def load_GroEL_ES():
+    # Load data
+    with open('../dataset/GroEl-ES.pickle', 'rb') as f:
+        data = pickle.load(f, encoding='latin1')
+
+    # Shuffle data
+    np.random.shuffle(data)
+
+    # Split data as defined in GumNet paper
+    num_training = 617
+    num_validation = 69
+    num_test = 100
+    train_data = data[:num_training]
+    valid_data = data[num_training:num_training + num_validation]
+    test_data = data[num_training + num_validation:num_training + num_validation + num_test]
+
+    return generate_data(train_data, valid_data, test_data)
+
+# Loads and generates usable torch data from Qiang's rat neuron culture dataset
+def load_rat_data():
+    with open('../dataset/qiang_train.pickle', 'rb') as f:
+        train_data = pickle.load(f, encoding='latin1')
+
+    with open('../dataset/qiang_test.pickle', 'rb') as f:
+        test_data = pickle.load(f, encoding='latin1')
+
+    with open('../dataset/qiang_valid.pickle', 'rb') as f:
+        valid_data = pickle.load(f, encoding='latin1')
+
+    return generate_data(train_data, valid_data, test_data)
